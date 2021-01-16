4d5b644f5983813e512e0f03b1088c90bde67095
Alay <artjj09@gmail.com>
2021-01-16 05:08:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './Screens/HomeScreen'
+//////all of the imports
 
 const App = () => {
   return (
