91a76016396d7ed4df3a73538fa2b44fd904ec0b
Alay <artjj09@gmail.com>
2021-01-17 13:04:24 -0700

    1/17

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
     return (
@@ -13,10 +14,7 @@ const Product = ({ product }) => {
                 </a>
 
                 <Card.Text as='div'>
-                    <div className='my-3'>
-                        {product.rating} from {product.numReviews} revies
-
-                    </div>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} color='#f8e825' />
                 </Card.Text>
 
                 <Card.Text as='h3'>${product.price}</Card.Text>
